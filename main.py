8b017ae24d07b06e6ddb4be23f3a4c8642fce461
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-05-16 21:42:37 +0200

    Adding score memory

--- main.py
+++ main.py
@@ -22,10 +22,8 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.right, "Right")
 screen.onkey(snake.left, "Left")
 
-
 game_on = True
 while game_on:
-
     screen.update()
     time.sleep(0.1)
     snake.move()
@@ -38,25 +36,16 @@ while game_on:
 
     # Detect collision with wall.
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_on = False
-        scoreboard.game_over()
+        scoreboard.reset_score()
+        snake.reset()
 
     # Detect collision with tail.
     for segment in snake.segments[1:]:
         # If head collides with any segment in the tail
-        if snake.head.distance(segment) < 1:
-            # trigger game over
-            scoreboard.game_over()
-            game_on = False
-
-
-
-
-
-
-
-
-
+        if snake.head.distance(segment) < 10:
+            scoreboard.reset_score()
+            snake.reset()
+        # trigger game over
 
 screen.exitonclick()
 
